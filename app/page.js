86269510002369447826d8272fe6dfa649a52897
Anthony Magpantay <anthony040190@yahoo.com>
2023-11-14 01:02:58 -0700

    delete some features from template

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Sidebar from './sidebar/sidebar'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-slate-950">
-      
+      <Sidebar />
     </main>
-  )
+  );
 }
