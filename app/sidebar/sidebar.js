3663eafd8496e220fd59a2b44a5208f1a9182e6e
jpm1203 <johnpatrick.mendoza@edu.sait.ca>
2023-11-23 09:31:43 -0700

    Update sidebar.js

--- app/sidebar/sidebar.js
+++ app/sidebar/sidebar.js
@@ -75,6 +75,15 @@ export default function Sidebar() {
                          <span className="flex-1 ms-3 whitespace-nowrap">To-Do List</span>
                         </a>
                     </li>
+
+                    <li>
+                        <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"  strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" viewBox="0 0 24 24">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                        </svg>
+                         <span className="flex-1 ms-3">About</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
             </aside>
