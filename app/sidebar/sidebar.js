fc9be47c789f0502737f7c7feff640cd3e48ba55
Anthony Magpantay <anthony040190@yahoo.com>
2023-12-07 00:52:13 -0700

    change name login to logout

--- app/sidebar/sidebar.js
+++ app/sidebar/sidebar.js
@@ -83,7 +83,7 @@ export default function Sidebar() {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none"  strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                         </svg>
-                         <span className="flex-1 ms-3 whitespace-nowrap">Login</span>
+                         <span className="flex-1 ms-3 whitespace-nowrap">Logout</span>
                         </a>
                     </li>
                 </ul>
