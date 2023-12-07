b1d79e8908bddc156d7ee653b54d7d976570b095
jpm1203 <johnpatrick.mendoza@edu.sait.ca>
2023-12-07 10:23:02 -0700

    check

---
+++ login.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Simulating asynchronous login process (replace with actual authentication logic)
+    setTimeout(() => {
+      if (username === 'your_username' && password === 'your_password') {
+        alert('Login successful!');
+      } else {
+        alert('Invalid username or password. Please try again.');
+      }
+    }, 500); // Simulating a delay for demonstration purposes
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Username:</Text>
+      <TextInput
+        style={styles.input}
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+        placeholder="Enter your username"
+      />
+
+      <Text style={styles.label}>Password:</Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Enter your password"
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 16,
+    padding: 8,
+  },
+  button: {
+    backgroundColor: '#4caf50',
+    padding: 10,
+    borderRadius: 4,
+    width: '100%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default Login;
